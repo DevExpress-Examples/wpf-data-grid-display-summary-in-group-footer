1228960a6da442dc0135b0816cb42cc9b0173601
DevExpressExampleBot <fakeEmail>
2023-12-09 18:59:34 +0400

    Source auto update [skip ci]

--- VB/GroupFootersWPF/GroupFooter.vb
+++ VB/GroupFootersWPF/GroupFooter.vb
@@ -1,91 +1,89 @@
-Imports Microsoft.VisualBasic
 Imports DevExpress.Xpf.Data
 Imports DevExpress.Xpf.Grid
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Windows
 Imports System.Windows.Controls
 
 Namespace GroupFootersWPF
-	Public Class GroupFooter
-		Inherits Control
-		Public Shared ReadOnly GroupRowDataProperty As DependencyProperty = DependencyProperty.Register("GroupRowData", GetType(MyGroupRowData), GetType(GroupFooter), New PropertyMetadata(Nothing))
-        Public Shared ReadOnly RowHandleProperty As DependencyProperty = DependencyProperty.Register("RowHandle", GetType(RowHandle), GetType(GroupFooter), New PropertyMetadata(New RowHandle(GridControl.InvalidRowHandle), AddressOf OnRowHandlePropertyChanged))
 
-        Public Shared Sub OnRowHandlePropertyChanged(d As DependencyObject, e As DependencyPropertyChangedEventArgs)
-            CType(d, GroupFooter).RefreshContent()
+    Public Class GroupFooter
+        Inherits Control
+
+        Public Shared ReadOnly GroupRowDataProperty As DependencyProperty = DependencyProperty.Register("GroupRowData", GetType(MyGroupRowData), GetType(GroupFooter), New PropertyMetadata(CType(Nothing, PropertyChangedCallback)))
+
+        Public Shared ReadOnly RowHandleProperty As DependencyProperty = DependencyProperty.Register("RowHandle", GetType(RowHandle), GetType(GroupFooter), New PropertyMetadata(New RowHandle(DataControlBase.InvalidRowHandle), Sub(d, e) CType(d, GroupFooter).RefreshContent()))
+
+        Public Sub New()
+            DefaultStyleKey = GetType(GroupFooter)
         End Sub
 
-		Public Sub New()
-			DefaultStyleKey = GetType(GroupFooter)
-		End Sub
+        Public Property GroupRowData As MyGroupRowData
+            Get
+                Return CType(GetValue(GroupRowDataProperty), MyGroupRowData)
+            End Get
 
-		Public Property GroupRowData() As MyGroupRowData
-			Get
-				Return CType(GetValue(GroupRowDataProperty), MyGroupRowData)
-			End Get
-			Set(ByVal value As MyGroupRowData)
-				SetValue(GroupRowDataProperty, value)
-			End Set
-		End Property
-		Public Property RowHandle() As RowHandle
-			Get
-				Return CType(GetValue(RowHandleProperty), RowHandle)
-			End Get
-			Set(ByVal value As RowHandle)
-				SetValue(RowHandleProperty, value)
-			End Set
-		End Property
+            Set(ByVal value As MyGroupRowData)
+                SetValue(GroupRowDataProperty, value)
+            End Set
+        End Property
 
-		Public ReadOnly Property View() As MyTableView
-			Get
-				Return If(DataContext Is Nothing, Nothing, CType((CType(DataContext, RowData)).View, MyTableView))
-			End Get
-		End Property
+        Public Property RowHandle As RowHandle
+            Get
+                Return CType(GetValue(RowHandleProperty), RowHandle)
+            End Get
 
-		Public Overrides Sub OnApplyTemplate()
-			MyBase.OnApplyTemplate()
-			RefreshContent()
-		End Sub
-		Public Function IsLastRowInGroup(ByVal groupRowHandle As Integer) As Boolean
-			Dim gridControl As GridControl = TryCast(View.DataControl, GridControl)
-			If (Not gridControl.IsValidRowHandle(groupRowHandle)) Then
-				Return False
-			End If
-			Dim childrenCount As Integer = gridControl.GetChildRowCount(groupRowHandle)
-			Dim lastRowHandle As Integer = gridControl.GetChildRowHandle(groupRowHandle, childrenCount - 1)
-			Return gridControl.GetRowVisibleIndexByHandle(lastRowHandle) <= gridControl.GetRowVisibleIndexByHandle(RowHandle.Value)
-		End Function
-		Public Sub RefreshContent()
-			If View Is Nothing Then
-				Return
-			End If
-			Dim groupRowHandle As RowHandle = GetParentRowHandle()
-			View.AddToCacheGroupFooter(Me)
-			UpdateVisibility(groupRowHandle)
-			If Visibility = Visibility.Collapsed Then
-				Return
-			End If
-			UpdateGroupFooterSummaryContent(groupRowHandle)
-			UpdateOffset()
-		End Sub
-		Private Sub UpdateOffset()
-			Dim offset As Double = View.LeftGroupAreaIndent * (CType(View.DataControl, GridControl)).GroupCount
-			Margin = New Thickness(-offset, 0, 0, 0)
-		End Sub
-		Private Sub UpdateGroupFooterSummaryContent(ByVal groupRowHandle As RowHandle)
-			If GroupRowData Is Nothing Then
-				GroupRowData = View.CreateGroupRowData(groupRowHandle)
-			Else
-				View.UpdateGroupRowData(GroupRowData, groupRowHandle)
-			End If
-		End Sub
-		Private Function GetParentRowHandle() As RowHandle
-			Return New RowHandle((CType(View.DataControl, GridControl)).GetParentRowHandle(RowHandle.Value))
-		End Function
-		Private Sub UpdateVisibility(ByVal groupRowHandle As RowHandle)
-			Visibility = If(IsLastRowInGroup(groupRowHandle.Value), Visibility.Visible, Visibility.Collapsed)
-		End Sub
-	End Class
+            Set(ByVal value As RowHandle)
+                SetValue(RowHandleProperty, value)
+            End Set
+        End Property
+
+        Public ReadOnly Property View As MyTableView
+            Get
+                Return If(DataContext Is Nothing, Nothing, CType(CType(DataContext, RowData).View, MyTableView))
+            End Get
+        End Property
+
+        Public Overrides Sub OnApplyTemplate()
+            MyBase.OnApplyTemplate()
+            RefreshContent()
+        End Sub
+
+        Public Function IsLastRowInGroup(ByVal groupRowHandle As Integer) As Boolean
+            Dim gridControl As GridControl = TryCast(View.DataControl, GridControl)
+            If Not gridControl.IsValidRowHandle(groupRowHandle) Then Return False
+            Dim childrenCount As Integer = gridControl.GetChildRowCount(groupRowHandle)
+            Dim lastRowHandle As Integer = gridControl.GetChildRowHandle(groupRowHandle, childrenCount - 1)
+            Return gridControl.GetRowVisibleIndexByHandle(lastRowHandle) <= gridControl.GetRowVisibleIndexByHandle(RowHandle.Value)
+        End Function
+
+        Public Sub RefreshContent()
+            If View Is Nothing Then Return
+            Dim groupRowHandle As RowHandle = GetParentRowHandle()
+            View.AddToCacheGroupFooter(Me)
+            UpdateVisibility(groupRowHandle)
+            If Visibility = Visibility.Collapsed Then Return
+            UpdateGroupFooterSummaryContent(groupRowHandle)
+            UpdateOffset()
+        End Sub
+
+        Private Sub UpdateOffset()
+            Dim offset As Double = View.LeftGroupAreaIndent * CType(View.DataControl, GridControl).GroupCount
+            Margin = New Thickness(-offset, 0, 0, 0)
+        End Sub
+
+        Private Sub UpdateGroupFooterSummaryContent(ByVal groupRowHandle As RowHandle)
+            If GroupRowData Is Nothing Then
+                GroupRowData = View.CreateGroupRowData(groupRowHandle)
+            Else
+                View.UpdateGroupRowData(GroupRowData, groupRowHandle)
+            End If
+        End Sub
+
+        Private Function GetParentRowHandle() As RowHandle
+            Return New RowHandle(CType(View.DataControl, GridControl).GetParentRowHandle(RowHandle.Value))
+        End Function
+
+        Private Sub UpdateVisibility(ByVal groupRowHandle As RowHandle)
+            Visibility = If(IsLastRowInGroup(groupRowHandle.Value), Visibility.Visible, Visibility.Collapsed)
+        End Sub
+    End Class
 End Namespace
--- VB/GroupFootersWPF/MainWindow.xaml.vb
+++ VB/GroupFootersWPF/MainWindow.xaml.vb
@@ -1,67 +1,32 @@
-﻿Imports Microsoft.VisualBasic
 Imports DevExpress.Xpf.Core
-Imports System
 Imports System.Collections.Generic
-Imports System.Linq
 
 Namespace GroupFootersWPF
-	Partial Public Class MainWindow
-		Inherits DXWindow
-		Public Sub New()
-			InitializeComponent()
-			Dim list As New List(Of TestData)()
-			For i As Integer = 0 To 199
-				list.Add(New TestData() With {.Text = "Text0 Row" & i, .Number0 = i, .Number1 = 100 - i, .Number2 = i Mod 10, .Group = i Mod 10})
-			Next i
-			DataContext = list
-		End Sub
-	End Class
 
-	Public Class TestData
-		Private privateText As String
-		Public Property Text() As String
-			Get
-				Return privateText
-			End Get
-			Set(ByVal value As String)
-				privateText = value
-			End Set
-		End Property
-		Private privateNumber0 As Integer
-		Public Property Number0() As Integer
-			Get
-				Return privateNumber0
-			End Get
-			Set(ByVal value As Integer)
-				privateNumber0 = value
-			End Set
-		End Property
-		Private privateNumber1 As Integer
-		Public Property Number1() As Integer
-			Get
-				Return privateNumber1
-			End Get
-			Set(ByVal value As Integer)
-				privateNumber1 = value
-			End Set
-		End Property
-		Private privateNumber2 As Integer
-		Public Property Number2() As Integer
-			Get
-				Return privateNumber2
-			End Get
-			Set(ByVal value As Integer)
-				privateNumber2 = value
-			End Set
-		End Property
-		Private privateGroup As Integer
-		Public Property Group() As Integer
-			Get
-				Return privateGroup
-			End Get
-			Set(ByVal value As Integer)
-				privateGroup = value
-			End Set
-		End Property
-	End Class
+    Public Partial Class MainWindow
+        Inherits DXWindow
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Dim list As List(Of TestData) = New List(Of TestData)()
+            For i As Integer = 0 To 200 - 1
+                list.Add(New TestData() With {.Text = "Text0 Row" & i, .Number0 = i, .Number1 = 100 - i, .Number2 = i Mod 10, .Group = i Mod 10})
+            Next
+
+            DataContext = list
+        End Sub
+    End Class
+
+    Public Class TestData
+
+        Public Property Text As String
+
+        Public Property Number0 As Integer
+
+        Public Property Number1 As Integer
+
+        Public Property Number2 As Integer
+
+        Public Property Group As Integer
+    End Class
 End Namespace
